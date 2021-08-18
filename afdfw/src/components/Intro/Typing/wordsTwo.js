033dc45e42fdfe57e71fef52c9ab842a52c96837
Kurtis Jin <cloudnine21@gmail.com>
2021-08-17 20:53:04 -0500

    added type.js. added intro. styled the intro. Need to figure out how to make the typing not move the welcome

---
+++ afdfw/src/components/Intro/Typing/wordsTwo.js
@@ -0,0 +1,8 @@
+const wordsTwo = [
+    `Are you a transplant? Feeling lonely?  
+    Want to meet asian people around your
+    area? You came to the right place!
+     `,
+    ]
+    
+    export default wordsTwo
--- afdfw/src/components/Intro/intro.js
+++ afdfw/src/components/Intro/intro.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import Typing from './Typing/Typing'
 
 let introPage = () => {
     return (
         <section id="introPage" className="introPage">
-            
-
+            <div className="max-width">
+                <div className="row animatedTitle">
+                    <div className="introPage-content">
+                        <div className="text-2 animate__animated animate__fadeInLeftBig"><span className="last-name">Welcome!</span></div>
+                    </div>
+                </div>
+                 <div className="text-3">
+                    <Typing />
+                    </div>    
+            </div>
         </section>
     )
 }
